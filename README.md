ddbd5f6520cda9268641bd7d04877b8d7ac9a057
archibate <1931127624@qq.com>
2024-01-29 16:24:03 +0800

    colorpick

---
+++ README.md
@@ -0,0 +1,23 @@
+# QDanmu
+
+跨平台的B站直播弹幕机，支持Linux/Windows/MacOS系统，快速查看自己直播间的B站弹幕
+
+## 特性
+
+- 扫码即可登录
+- 半透明悬浮窗口
+- 窗口全屏游戏中依然可见
+- 显示当前正在播放音乐名
+- 可供 OBS 实时读取显示
+- 颜色、透明度、字体、窗口位置等均可配置
+
+未来可能会加入的特性：
+
+- 支持发送弹幕
+
+## 安装与使用
+
+```bash
+python -m pip install -r requirements.txt
+python danmu.py
+```
--- danmu.py
+++ danmu.py
@@ -9,6 +9,7 @@ import traceback
 import threading
 import tempfile
 import requests
+import hashlib
 import queue
 import json
 import time
@@ -16,13 +17,13 @@ import sys
 import os
 import re
 
-# with open(os.path.expanduser('~/.bilibili-cookies.json'), 'r') as f:
-#     cookies = {item['name']: item['value'] for item in json.load(f)}
+with open(os.path.realpath(__file__), 'rb') as f:
+    current_version = hashlib.md5(f.read()).hexdigest()[:8]
 
 headers = {
     'Accept': '*/*',
     'Accept-Language': 'en-US,en;q=0.5',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36',
 }
 cookies = {}
 
@@ -32,25 +33,34 @@ if os.path.exists('.bilibili-options.json'):
 else:
     options = {}
 
-if len(options) == 0:
+if len(options) == 0 or options.get('version', 'unknown') != current_version:
+    print(f'Initializing options (version {current_version})')
     options = {
+        'version': current_version,
         'width': 420,
         'height': 120,
         'refreshInterval': 6,
         'backgroundOpacity': 0.15,
         'foregroundOpacity': 0.35,
-        'fontColor': 'black',
-        'windowLocation': 'bottomLeft',
+        'fontSize': 18,
+        'foregroundR': 0,
+        'foregroundG': 0,
+        'foregroundB': 0,
+        'backgroundR': 255,
+        'backgroundG': 255,
+        'backgroundB': 255,
+        'windowLocation': '左下角',
         'showMusicName': True,
         'danmuFile': tempfile.gettempdir() + '/danmu.txt',
         'danmuFormat': '{danmu}\n[B站弹幕有屏蔽词，没显示就是叔叔屏蔽了]\n[已知屏蔽词：小彭老师、皇帝卡、Electron]',
+        'musicRegex': '( - VLC media player|_哔哩哔哩_bilibili — Mozilla Firefox)$',
     }
 
 def current_music():
     if sys.platform != 'linux':
-        return ''
+        return ''  # 暂不支持其他系统的当前音乐查询，如果你知道如何获取窗口标题的列表，欢迎PR
     try:
-        suffix = r'( - VLC media player|_哔哩哔哩_bilibili — Mozilla Firefox)$'
+        suffix = options['musicRegex']
         with subprocess.Popen(['wmctrl', '-l'], stdin=subprocess.DEVNULL, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as p:
             stdout, _ = p.communicate()
             stdout = stdout.decode()
@@ -73,40 +83,6 @@ def current_music():
         traceback.print_exc()
         return ''
 
-# def login():
-#     url = 'https://passport.bilibili.com/x/passport-login/web/qrcode/generate'
-#     req = requests.get(url, headers=headers)
-#     cookies.update(req.cookies.get_dict())
-#     data = json.loads(req.text)
-#     qr_url = data['data']['url']
-#     qrcode_key = data['data']['qrcode_key']
-#     print(qrcode_key, qr_url)
-#     import qrcode
-#     img = qrcode.make(qr_url)
-#     img.show()
-#     while True:
-#         url = f'https://passport.bilibili.com/x/passport-login/web/qrcode/poll?qrcode_key={qrcode_key}'
-#         req = requests.get(url, headers=headers, cookies=cookies)
-#         print(req.text, req.cookies.get_dict())
-#         cookies.update(req.cookies.get_dict())
-#         data = json.loads(req.text)
-#         data = data['data']
-#         if data['code'] == 0:
-#             print('登录成功')
-#             break
-#         elif data['code'] == 86038:
-#             print('二维码已过期')
-#             break
-#         elif data['code'] == 86090:
-#             print('请在手机App上确认登录')
-#         elif data['code'] == 86101:
-#             print('请在手机App上扫描二维码')
-#         else:
-#             print(data['message'])
-#         time.sleep(3)
-#     with open('.bilibili-cookies.json', 'w') as f:
-#         json.dump(cookies, f)
-
 class MyThread(QThread):
     def __init__(self, parent, func, *args, **kwargs):
         super().__init__(parent)
@@ -128,7 +104,7 @@ class LoginWindow(QWidget):
         self.image.setScaledContents(True)
         self.image.setPixmap(QPixmap('icon.png').scaled(400, 400, Qt.KeepAspectRatio))
 
-        self.label = QLabel('欢迎使用弹幕助手')
+        self.label = QLabel('欢迎使用小彭老师弹幕助手')
 
         layout = QVBoxLayout()
         layout.setContentsMargins(0, 0, 0, 0)
@@ -224,8 +200,7 @@ def get_messages():
         req = requests.get(url, headers=headers, cookies=cookies)
         data = json.loads(req.text)['data']
         roomid = data['roomid']
-        # title = data['title']
-        # print(f'已进入直播间 {title} ({roomid})')
+        # print(f'已进入直播间 {data['title']} ({roomid})')
         set_option('roomId', roomid)
     else:
         roomid = options['roomId']
@@ -246,11 +221,11 @@ def set_option(key, value):
         json.dump(options, f)
 
 class SettingsWindow(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, master, parent=None):
         super().__init__(parent)
+        self.master = master
 
-        icon = QIcon('icon.png')
-        self.setWindowIcon(icon)
+        self.setWindowIcon(self.master.icon)
         self.setWindowTitle('弹幕助手')
         self.resize(400, 400)
 
@@ -260,73 +235,144 @@ class SettingsWindow(QWidget):
         layout.setContentsMargins(0, 0, 0, 0)
         layout.addWidget(self.login_window)
         hlayout = QHBoxLayout()
-        button = QPushButton('登录')
+        button = QPushButton('登录B站账号')
         button.clicked.connect(self.login_window.login)
         hlayout.addWidget(button)
-        button = QPushButton('退出')
+        button = QPushButton('退出弹幕助手')
         button.clicked.connect(QApplication.quit)
         hlayout.addWidget(button)
-        button = QPushButton('重启')
+        layout.addLayout(hlayout)
+        hlayout = QHBoxLayout()
+        button = QPushButton('最小化到托盘')
+        button.clicked.connect(self.close)
+        hlayout.addWidget(button)
+        button = QPushButton('隐藏弹幕窗口')
+        button.clicked.connect((lambda button: lambda: button.setText('显示弹幕窗口' if self.master.toggle_window() else '隐藏弹幕窗口'))(button))
+        hlayout.addWidget(button)
+        layout.addLayout(hlayout)
+        hlayout = QHBoxLayout()
+        hlayout.addWidget(QLabel('窗口大小(宽x高)'))
+        w = QSpinBox()
+        w.setRange(100, 1000)
+        w.setValue(options['width'])
+        w.valueChanged.connect(lambda value: set_option('width', value))
+        hlayout.addWidget(w)
+        w = QSpinBox()
+        w.setRange(100, 1000)
+        w.setValue(options['height'])
+        w.valueChanged.connect(lambda value: set_option('height', value))
+        hlayout.addWidget(w)
+        layout.addLayout(hlayout)
+        hlayout = QHBoxLayout()
+        hlayout.addWidget(QLabel('弹幕刷新间隔(秒)'))
+        w = QSpinBox()
+        w.setRange(1, 60)
+        w.setValue(options['refreshInterval'])
+        w.valueChanged.connect(lambda value: set_option('refreshInterval', value))
+        hlayout.addWidget(w)
+        layout.addLayout(hlayout)
+        hlayout = QHBoxLayout()
+        hlayout.addWidget(QLabel('字体大小(px)'))
+        w = QSpinBox()
+        w.setRange(1, 100)
+        w.setValue(options['fontSize'])
+        w.valueChanged.connect(lambda value: set_option('fontSize', value))
+        hlayout.addWidget(w)
+        layout.addLayout(hlayout)
+        hlayout = QHBoxLayout()
+        hlayout.addWidget(QLabel('前景透明度(%)'))
+        w = QSpinBox()
+        w.setRange(0, 100)
+        w.setValue(int(options['foregroundOpacity'] * 100))
+        w.valueChanged.connect(lambda value: set_option('foregroundOpacity', value / 100))
+        hlayout.addWidget(w)
+        hlayout.addWidget(QLabel('背景透明度(%)'))
+        w = QSpinBox()
+        w.setRange(0, 100)
+        w.setValue(int(options['backgroundOpacity'] * 100))
+        w.valueChanged.connect(lambda value: set_option('backgroundOpacity', value / 100))
+        hlayout.addWidget(w)
+        layout.addLayout(hlayout)
+        hlayout = QHBoxLayout()
+        hlayout.addWidget(QLabel('前景颜色'))
+        w = QPushButton()
+        w.setStyleSheet(f'padding: 0px; background-color: rgb({options["foregroundR"]}, {options["foregroundG"]}, {options["foregroundB"]});')
+        w.clicked.connect(self.color_picker(w, 'foreground'))
+        hlayout.addWidget(w)
+        hlayout.addWidget(QLabel('背景颜色'))
+        w = QPushButton()
+        w.setStyleSheet(f'padding: 0px; background-color: rgb({options["backgroundR"]}, {options["backgroundG"]}, {options["backgroundB"]});')
+        w.clicked.connect(self.color_picker(w, 'background'))
+        hlayout.addWidget(w)
+        layout.addLayout(hlayout)
+        hlayout = QHBoxLayout()
+        hlayout.addWidget(QLabel('窗口位置'))
+        w = QComboBox()
+        w.addItems(['左上角', '右上角', '左下角', '右下角'])
+        w.setCurrentText(options['windowLocation'])
+        w.currentTextChanged.connect(lambda value: set_option('windowLocation', value))
+        hlayout.addWidget(w)
+        layout.addLayout(hlayout)
+        hlayout = QHBoxLayout()
+        w = QCheckBox('音乐软件正则表达式')
+        w.setChecked(options['showMusicName'])
+        w.stateChanged.connect(lambda value: set_option('showMusicName', value))
+        hlayout.addWidget(w)
+        w = QLineEdit()
+        w.setText(options['musicRegex'])
+        w.textChanged.connect(lambda value: set_option('musicRegex', value))
+        hlayout.addWidget(w)
+        layout.addLayout(hlayout)
+        hlayout = QHBoxLayout()
+        hlayout.addWidget(QLabel('弹幕输出文件(可供OBS使用)'))
+        w = QLineEdit()
+        w.setText(options['danmuFile'])
+        w.textChanged.connect(lambda value: set_option('danmuFile', value))
+        hlayout.addWidget(w)
+        layout.addLayout(hlayout)
+        hlayout = QHBoxLayout()
+        hlayout.addWidget(QLabel('弹幕输出格式'))
+        w = QTextEdit()
+        w.setAcceptRichText(False)
+        w.setFixedHeight(60)
+        w.setText(options['danmuFormat'])
+        w.textChanged.connect(lambda: set_option('danmuFormat', w.toPlainText()))
+        hlayout.addWidget(w)
+        layout.addLayout(hlayout)
+        hlayout = QHBoxLayout()
+        button = QPushButton('恢复出厂设置')
+        button.clicked.connect(lambda: set_option('version', 'restore') or self.restart())
+        hlayout.addWidget(button)
+        button = QPushButton('应用并重启')
         button.clicked.connect(self.restart)
         hlayout.addWidget(button)
         layout.addLayout(hlayout)
-        layout.addWidget(QLabel('窗口大小(宽x高)'))
-        self.width = QSpinBox()
-        self.width.setRange(100, 1000)
-        self.width.setValue(options['width'])
-        self.width.valueChanged.connect(lambda value: set_option('width', value))
-        self.height = QSpinBox()
-        self.height.setRange(100, 1000)
-        self.height.setValue(options['height'])
-        self.height.valueChanged.connect(lambda value: set_option('height', value))
-        layout.addWidget(self.width)
-        layout.addWidget(self.height)
-        layout.addWidget(QLabel('刷新间隔(秒)'))
-        self.refreshInterval = QSpinBox()
-        self.refreshInterval.setRange(1, 60)
-        self.refreshInterval.setValue(options['refreshInterval'])
-        self.refreshInterval.valueChanged.connect(lambda value: set_option('refreshInterval', value))
-        layout.addWidget(self.refreshInterval)
-        layout.addWidget(QLabel('背景透明度(%)'))
-        self.backgroundOpacity = QSpinBox()
-        self.backgroundOpacity.setRange(0, 100)
-        self.backgroundOpacity.setValue(int(options['backgroundOpacity'] * 100))
-        self.backgroundOpacity.valueChanged.connect(lambda value: set_option('backgroundOpacity', value / 100))
-        layout.addWidget(self.backgroundOpacity)
-        layout.addWidget(QLabel('前景透明度(%)'))
-        self.foregroundOpacity = QSpinBox()
-        self.foregroundOpacity.setRange(0, 100)
-        self.foregroundOpacity.setValue(int(options['foregroundOpacity'] * 100))
-        self.foregroundOpacity.valueChanged.connect(lambda value: set_option('foregroundOpacity', value / 100))
-        self.showMusicName = QCheckBox('显示当前音乐')
-        self.showMusicName.setChecked(options['showMusicName'])
-        self.showMusicName.stateChanged.connect(lambda value: set_option('showMusicName', value))
-        layout.addWidget(self.showMusicName)
-        layout.addWidget(QLabel('字体颜色'))
-        self.fontColor = QComboBox()
-        self.fontColor.addItems(['black', 'white'])
-        self.fontColor.setCurrentText(options['fontColor'])
-        self.fontColor.currentTextChanged.connect(lambda value: set_option('fontColor', value))
-        layout.addWidget(self.fontColor)
-        layout.addWidget(QLabel('窗口位置'))
-        self.windowLocation = QComboBox()
-        self.windowLocation.addItems(['topLeft', 'topRight', 'bottomLeft', 'bottomRight'])
-        self.windowLocation.setCurrentText(options['windowLocation'])
-        self.windowLocation.currentTextChanged.connect(lambda value: set_option('windowLocation', value))
-        layout.addWidget(self.windowLocation)
-        layout.addWidget(QLabel('弹幕输出文件(可供OBS等软件使用)'))
-        self.danmuFile = QLineEdit()
-        self.danmuFile.setText(options['danmuFile'])
-        self.danmuFile.textChanged.connect(lambda value: set_option('danmuFile', value))
-        layout.addWidget(self.danmuFile)
-        layout.addWidget(QLabel('弹幕文本输出格式'))
-        self.danmuFormat = QTextEdit()
-        self.danmuFormat.setAcceptRichText(False)
-        self.danmuFormat.setFixedHeight(60)
-        self.danmuFormat.setText(options['danmuFormat'])
-        self.danmuFormat.textChanged.connect(lambda: set_option('danmuFormat', self.danmuFormat.toPlainText()))
-        layout.addWidget(self.danmuFormat)
         self.setLayout(layout)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Escape:
+            self.close()
+
+    def closeEvent(self, a0):
+        event = a0
+        assert event
+        event.ignore()
+        self.hide()
+        self.master.tray_icon.showMessage('弹幕助手仍在运行',
+            '程序已最小化到托盘，右键托盘图标 ->"设置/登录" 可以重新打开。',
+            self.master.icon, 2000)
+
+    def color_picker(self, button, which):
+        def wrapped():
+            color = QColor(options[f"{which}R"], options[f"{which}G"], options[f"{which}B"])
+            color = QColorDialog.getColor(color)
+            if not color.isValid():
+                return
+            set_option(f'{which}R', color.red())
+            set_option(f'{which}G', color.green())
+            set_option(f'{which}B', color.blue())
+            button.setStyleSheet(f'padding: 0px; background-color: rgb({options[f"{which}R"]}, {options[f"{which}G"]}, {options[f"{which}B"]});')
+        return wrapped
 
     def restart(self):
         os.execl(sys.executable, sys.executable, *sys.argv)
@@ -335,20 +381,21 @@ class MainWindow(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        icon = QIcon('icon.png')
-        self.setWindowIcon(icon)
+        self.icon = QIcon('icon.png')
+        self.setWindowIcon(self.icon)
         self.setWindowTitle('弹幕助手')
         # self.setWindowOpacity(0.95)
-        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.X11BypassWindowManagerHint)
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | (0 if sys.platform != 'linux' else Qt.X11BypassWindowManagerHint))
         self.setAttribute(Qt.WA_NoSystemBackground)
         self.setAttribute(Qt.WA_TranslucentBackground)
         self.setAttribute(Qt.WA_TransparentForMouseEvents)
 
-        self.settings_window = SettingsWindow()
+        self.settings_window = SettingsWindow(self)
+        self.settings_window.show()
 
-        tray_icon = QSystemTrayIcon(self)
-        tray_icon.setIcon(icon)
-        tray_icon.setToolTip('弹幕助手')
+        self.tray_icon = QSystemTrayIcon(self)
+        self.tray_icon.setIcon(self.icon)
+        self.tray_icon.setToolTip('弹幕助手')
 
         tray_menu = QMenu(self)
         action = tray_menu.addAction("显示/隐藏")
@@ -358,33 +405,34 @@ class MainWindow(QWidget):
         action = tray_menu.addAction("退出")
         action.triggered.connect(QApplication.quit)
 
-        tray_icon.setContextMenu(tray_menu)
-        tray_icon.activated.connect(self.toggle_window)
-        tray_icon.show()
+        self.tray_icon.setContextMenu(tray_menu)
+        self.tray_icon.activated.connect(self.toggle_window)
+        self.tray_icon.show()
 
         w, h = options['width'], options['height']
         self.resize(w, h)
 
         desktop = QApplication.desktop()
-        if options['windowLocation'] == 'bottomRight':
+        if options['windowLocation'] == '右下角':
             bottom_right = desktop.screenGeometry().bottomRight()
             self.move(bottom_right - QPoint(w, h))
-        elif options['windowLocation'] == 'bottomLeft':
+        elif options['windowLocation'] == '左下角':
             bottom_left = desktop.screenGeometry().bottomLeft()
             self.move(bottom_left - QPoint(0, h))
-        elif options['windowLocation'] == 'topRight':
+        elif options['windowLocation'] == '右上角':
             top_right = desktop.screenGeometry().topRight()
             self.move(top_right - QPoint(w, 0))
-        elif options['windowLocation'] == 'topLeft':
+        elif options['windowLocation'] == '左上角':
             top_left = desktop.screenGeometry().topLeft()
             self.move(top_left)
 
-        bga, fga = options['backgroundOpacity'], options['foregroundOpacity']
-        bgc, fgc = {
-            'black': (255, 0),
-            'white': (0, 255),
-        }[options['fontColor']]
-        self.setStyleSheet(f'* {{font-size: 18px; color: rgba({fgc}, {fgc}, {fgc}, {fga}); background-color: rgba({bgc}, {bgc}, {bgc}, {bga}); border-radius: 10px; padding: 0px;}}')
+        self.setStyleSheet(f"""* {{
+    border-radius: 10px;
+    padding: 0px;
+    font-size: {options['fontSize']}px;
+    color: rgba({options['foregroundR']}, {options['foregroundG']}, {options['foregroundB']}, {options['foregroundOpacity']});
+    background-color: rgba({options['backgroundR']}, {options['backgroundG']}, {options['backgroundB']}, {options['backgroundOpacity']});
+}}""")
 
         self.lv = QListView()
         self.lv.setSelectionMode(QAbstractItemView.NoSelection)
@@ -414,8 +462,10 @@ class MainWindow(QWidget):
     def toggle_window(self):
         if self.isVisible():
             self.hide()
+            return True
         else:
             self.show()
+            return False
 
     def message_worker(self):
         while True:
@@ -467,5 +517,4 @@ def main():
 
 
 if __name__ == '__main__':
-    # print(current_music())
     sys.exit(main())
